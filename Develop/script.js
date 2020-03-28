b8da6ec7f39f2d54d150799f0f43fcf3851f24a1
BrandonKutivan <bskutivan@gmail.com>
2020-03-27 20:00:37 -0700

    Added constants for uppercase, lower case, number, and special characters. Built arrayFromLowToHigh function to cycle through character codes and build array of them that will define the constants.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,55 @@
 // Assignment code here
 
+// Character Codes for Lowercase, Uppercase, & Symbols
+const upperCaseCharCodes = arrayFromLowToHigh(65, 72).concat(
+  arrayFromLowToHigh(74, 78)
+).concat(
+    arrayFromLowToHigh(80, 90)
+)
+const lowerCaseCharCodes = arrayFromLowToHigh(97, 107).concat(
+  arrayFromLowToHigh(109, 122)
+)
+const numberCharCodes = arrayFromLowToHigh(48, 57)
+const specialCharCodes = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function generatePassword(charAmount, uppercaseConfirm, lowercaseConfirm, numbersConfirm, symbolsConfirm) {
+// Prompt to ask how many characters
+  
+  // capture response
+  // if < 8, prompt user to put in a number greater than 8 and < 45
+  // return to start of function to get correct input
+
+
+
+// Confirm for lowercase
+  // capture response
+
+// Confirm for uppercase
+  // capture response
+
+// Confirm for numeric
+  // capture response
+
+// Confirm for special characters
+  // capture response
+};
+
+// generatePassword();
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
